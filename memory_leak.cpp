a25f00f371d33d6d17f7868265780a1cf64ef805
juhoon212 <ws5501@naver.com>
2024-04-11 10:59:19 +0900

    memory-leak

---
+++ memory_leak.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+
+    // int allocCount = 0;
+    // std::cout << "how many" << std::endl;
+    // std::cin >> allocCount;
+
+    // for (int i = 0; i < allocCount; i++) {
+        
+    //     int* ip = new int[250];
+    //     std::cout << "mem " << std::flush;
+    //     delete [] ip;
+    // }
+
+    // return 0;
+
+    int* ip = new int[250]; // 1kb
+    return 0;
+}
