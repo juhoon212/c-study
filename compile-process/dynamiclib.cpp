ba1a97837fcac32b00bc0f7ac72986dcab6c98d7
juhoon212 <ws5501@naver.com>
2024-04-19 09:53:13 +0900

    pointer vs reference

--- compile-process/dynamiclib.cpp
+++ compile-process/dynamiclib.cpp
@@ -2,4 +2,7 @@
 
 void foo() {
     std::cout << "foo" << std::endl;
+
+    // -fPIC 옵션은 shared library에 절대 주소가 아닌 relative 주소를 반양하여 각각 다른 어플리케이션에서 쓸 수 있게 끔 만들어주는 옵션이다.
+    // clang++ -shared dynamiclib.o -o libdynamic.so => shared library 
 }
