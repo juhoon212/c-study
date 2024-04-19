fea7f90160570faa9d7ecb829a1a8b7bfe1c7a62
juhoon212 <ws5501@naver.com>
2024-04-19 10:29:36 +0900

    value vs ref

---
+++ resource-move/value.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+
+// l value => 1번 이상 불러줄수 있으면
+// r value => temporary, 다시 쓸 수 없으면
+// l value를 std::move() 를 사용하면 r value로 변환할 수 있음.
+
+void storeByValue(std::string s) {
+    std::string b = s;
+}
+
+void storeByLRef(std::string &s) {
+    std::string b = s;
+}
+
+void storeByRRef(std::string && s) {
+    std::string b = std::move(s); // wrong!
+    std::cout << b << std::endl;
+}
+
+int main() {
+    // std::string a = "abc";
+    // storeByValue(a);
+    // storeByLRef(a);
+    storeByRRef("abc"); // wrong!
+}
