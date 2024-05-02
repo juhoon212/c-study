25d63c9752bde792a2091eb8d7e755a760a13f7b
juhoon212 <ws5501@naver.com>
2024-05-02 10:13:08 +0900

    c

---
+++ about_c/자료형.c
@@ -0,0 +1,13 @@
+// char => 1byte
+// short => 2byte
+// int => 4byte(32bit)
+// long => 4byte (32bit) => (64bit) 8byte
+// long long => 8byte
+// long long int => 4byte (32bit)
+// unsigned 조합 => 부호비트도 데이터 비트로 해석하는 부호가 없는 정수형
+
+// float => 4byte
+// double => 8byte
+// long double => 8byte 이상 32bit => 12byte 64bit => 16byte
+// size_t , ssize_t 4byte 64bit => 8byte
+// * => 4byte(32bit) , 8byte (64bit)
