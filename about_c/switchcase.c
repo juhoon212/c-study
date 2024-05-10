0dfa6e7a6a55d4a45d88912ec515eefae4ec4fd9
juhoon212 <ws5501@naver.com>
2024-05-10 09:47:15 +0900

    switch case

---
+++ about_c/switchcase.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void) {
+
+
+    int nInput = 0;
+    char chCredit = 'X';
+    scanf("%d", &nInput);
+
+    switch (nInput / 10) {
+        case 10:
+        case 9:
+            chCredit = 'A';
+            break;
+        case 8:
+            chCredit = 'B';
+            break;
+        case 7:
+            chCredit = 'C';
+            break;
+        case 6:
+            chCredit = 'D';
+            break;
+        default:
+            chCredit = 'F';
+    }
+
+    printf("Credit : %c\n", chCredit);
+
+    return 0;
+}
