4de2a32079f1c84afbebd6c4c99d716ae201c87e
juhoon212 <ws5501@naver.com>
2024-06-19 11:10:50 +0900

    DoubleLinkedList

--- 자료구조/DoubleLinkedList.c
+++ 자료구조/DoubleLinkedList.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 typedef struct NODE {
-    char* pszData;
+    char pszData[64];
     struct NODE* prev;
     struct NODE* next;
 } NODE;
@@ -17,21 +17,78 @@ void initList(void) {
     g_pTail = (NODE*)malloc(sizeof(NODE));
     g_nSize = 0;
 
+    memset(g_pHead, 0, sizeof(NODE));
+    memset(g_pTail, 0, sizeof(NODE)); 
+
+    strcpy(g_pHead->pszData, "HEAD");
+    strcpy(g_pTail->pszData, "TAIL");
+    
+    g_pHead->next = g_pTail;
+    g_pTail->prev = g_pHead;
 }
 
 void releaseList(void) {
-    
+    NODE* pTemp = g_pHead;
+
+    while (pTemp != NULL) {
+        NODE* pDelete = pTemp;
+        pTemp = pTemp->next;
+
+        printf("free : [%p] , %s\n", pDelete, pDelete->pszData);
+
+        free(pDelete);
+    }
+
+    g_pHead = NULL;
+    g_pTail = NULL;
+    g_nSize = 0;
+    printf("ReleaseList()\n");
 }
 
+
+
 void printList(void) {
-    
+
+    printf("printList() : g_nSize [%d] , g_pHead [%p], g_pTail [%p]\n", g_nSize, g_pHead, g_pTail);
+
+    NODE* pTemp = g_pHead;
+
+    while (pTemp != NULL) {
+        printf("prev : [%p] , %s, next : [%p]\n", pTemp->prev, pTemp->pszData, pTemp->next);
+        pTemp = pTemp->next;
+    }
 }
 
 int insertAtHead(const char* pszData) {
-    return 0;
+
+    NODE* pNewNode = (NODE*)malloc(sizeof(NODE));
+    memset(pNewNode, 0, sizeof(NODE));
+    strcpy(pNewNode->pszData, pszData);
+
+    pNewNode->next = g_pHead->next;
+    pNewNode->prev = g_pHead;
+
+    g_pHead->next = pNewNode;
+    pNewNode->next->prev = pNewNode;
+
+    g_nSize++;
+
+    return g_nSize;
 }
 
 int insertAtTail(const char* pszData) {
+
+    NODE* pNewNode = (NODE*)malloc(sizeof(NODE));
+    memset(pNewNode, 0, sizeof(NODE));
+
+    strcpy(pNewNode->pszData, pszData);
+
+    pNewNode->next = g_pTail;
+    pNewNode->prev = g_pTail->prev;
+
+    g_pTail->prev = pNewNode;
+    pNewNode->prev->next = pNewNode;
+
     return 0;
 }
 
@@ -55,9 +112,21 @@ int getLength(void) {
     return getSize();
 }
 
+int isEmpty(void) {
+    return 0;
+}
+
 int main(void) {
 
-    
+    initList();
+
+    insertAtHead("TEST01");
+    insertAtHead("TEST02");
+    insertAtHead("TEST03");
+
+    printList();
+
+    releaseList();
 
     return 0;
 }
