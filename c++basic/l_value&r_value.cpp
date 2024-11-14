a8fc6157b9e651bdcdb8b960f82bbaf9de25cef4
juhoon212 <ws5501@naver.com>
2024-11-14 22:00:39 +0900

    l_value && r_value

---
+++ c++basic/l_value&r_value.cpp
@@ -0,0 +1,14 @@
+// l-value = callable
+// r-value = temp
+
+#include <iostream>
+#include <string>
+
+int main(void) {
+    std::string a = "nocope";
+    std::string b = std::move(a);
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+}
+
